9c003f979ae0fe27b1fcb18f986ab2e176d43df9
José Francisco Monroy Ugarte <31453445+pacomonroy@users.noreply.github.com>
2025-08-08 13:08:36 -0600

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,9 +4,8 @@ import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: process.env.SITE || 'https://www.jojuvo.com',
+  site: process.env.SITE || 'https://www.jojuvo.com', // put your real domain
+  base: '/',                    // if deploying to root; use '/sub' only if deploying to a subfolder
   integrations: [react(), tailwind(), sitemap()],
   prefetch: true,
 });
-
-export default defineConfig({ site: 'https://yourdomain.com/sub', base: '/sub' });
